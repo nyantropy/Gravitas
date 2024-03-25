536bb676b0618ca9b28f45cf71891cb36540c497
nyantropy <>
2024-03-25 22:58:14 +0100

    removed build folder, added it to gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/entropy/Desktop/work/Gravitas
-# Build directory: /home/entropy/Desktop/work/Gravitas/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
