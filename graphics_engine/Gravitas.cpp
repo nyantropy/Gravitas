1fca5147419dbe7932e4704ef3159e41e095f8a7
nyantropy <>
2024-06-17 12:27:22 +0200

    moved some more code to vertex

--- graphics_engine/Gravitas.cpp
+++ graphics_engine/Gravitas.cpp
@@ -72,17 +72,6 @@ const bool enableValidationLayers = true;
 //     }
 // }
 
-namespace std 
-{
-    template<> struct hash<Vertex> 
-    {
-        size_t operator()(Vertex const& vertex) const 
-        {
-            return ((hash<glm::vec3>()(vertex.pos) ^ (hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^ (hash<glm::vec2>()(vertex.texCoord) << 1);
-        }
-    };
-}
-
 class Gravitas {
 public:
     GTSOutputWindow* vwindow;
--- graphics_engine/Vertex.h
+++ graphics_engine/Vertex.h
@@ -46,3 +46,14 @@ struct Vertex
         return pos == other.pos && color == other.color && texCoord == other.texCoord;
     }
 };
+
+namespace std 
+{
+    template<> struct hash<Vertex> 
+    {
+        size_t operator()(Vertex const& vertex) const 
+        {
+            return ((hash<glm::vec3>()(vertex.pos) ^ (hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^ (hash<glm::vec2>()(vertex.texCoord) << 1);
+        }
+    };
+}
