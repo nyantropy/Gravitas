8d1573773a5418cf670e4d1ac6300ef315b99dc5
nyantropy <>
2024-06-17 12:26:17 +0200

    created UniformBufferObject and Vertex header files

--- graphics_engine/Gravitas.cpp
+++ graphics_engine/Gravitas.cpp
@@ -30,6 +30,8 @@
 #include <unordered_map>
 
 #include "GravitasEngineConstants.h"
+#include "Vertex.h"
+#include "UniformBufferObject.h"
 
 #include "GTSGLFWOutputWindow.hpp"
 #include "GLFWWindowSurface.hpp"
@@ -70,59 +72,16 @@ const bool enableValidationLayers = true;
 //     }
 // }
 
-struct Vertex {
-    glm::vec3 pos;
-    glm::vec3 color;
-    glm::vec2 texCoord;
-
-    static VkVertexInputBindingDescription getBindingDescription() {
-        VkVertexInputBindingDescription bindingDescription{};
-        bindingDescription.binding = 0;
-        bindingDescription.stride = sizeof(Vertex);
-        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-
-        return bindingDescription;
-    }
-
-    static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions() {
-        std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions{};
-
-        attributeDescriptions[0].binding = 0;
-        attributeDescriptions[0].location = 0;
-        attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attributeDescriptions[0].offset = offsetof(Vertex, pos);
-
-        attributeDescriptions[1].binding = 0;
-        attributeDescriptions[1].location = 1;
-        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attributeDescriptions[1].offset = offsetof(Vertex, color);
-
-        attributeDescriptions[2].binding = 0;
-        attributeDescriptions[2].location = 2;
-        attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
-        attributeDescriptions[2].offset = offsetof(Vertex, texCoord);
-
-        return attributeDescriptions;
-    }
-
-    bool operator==(const Vertex& other) const {
-        return pos == other.pos && color == other.color && texCoord == other.texCoord;
-    }
-};
-
-namespace std {
-    template<> struct hash<Vertex> {
-        size_t operator()(Vertex const& vertex) const {
+namespace std 
+{
+    template<> struct hash<Vertex> 
+    {
+        size_t operator()(Vertex const& vertex) const 
+        {
             return ((hash<glm::vec3>()(vertex.pos) ^ (hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^ (hash<glm::vec2>()(vertex.texCoord) << 1);
         }
     };
 }
-
-struct UniformBufferObject {
-    alignas(16) glm::mat4 model;
-    alignas(16) glm::mat4 view;
-    alignas(16) glm::mat4 proj;
-};
 
 class Gravitas {
 public:
@@ -332,66 +291,6 @@ private:
     //     createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
     //     createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
     //     createInfo.pfnUserCallback = debugCallback;
-    // }
-
-    // void createRenderPass() 
-    // {
-    //     VkAttachmentDescription colorAttachment{};
-    //     colorAttachment.format = vswapchain->getSwapChainImageFormat();
-    //     colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
-    //     colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-    //     colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-    //     colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-    //     colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-    //     colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-    //     colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
-
-    //     VkAttachmentDescription depthAttachment{};
-    //     depthAttachment.format = findDepthFormat();
-    //     depthAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
-    //     depthAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-    //     depthAttachment.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-    //     depthAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-    //     depthAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-    //     depthAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-    //     depthAttachment.finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-
-    //     VkAttachmentReference colorAttachmentRef{};
-    //     colorAttachmentRef.attachment = 0;
-    //     colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
-
-    //     VkAttachmentReference depthAttachmentRef{};
-    //     depthAttachmentRef.attachment = 1;
-    //     depthAttachmentRef.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-
-    //     VkSubpassDescription subpass{};
-    //     subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
-    //     subpass.colorAttachmentCount = 1;
-    //     subpass.pColorAttachments = &colorAttachmentRef;
-    //     subpass.pDepthStencilAttachment = &depthAttachmentRef;
-
-    //     VkSubpassDependency dependency{};
-    //     dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
-    //     dependency.dstSubpass = 0;
-    //     dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;
-    //     dependency.srcAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
-    //     dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
-    //     dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
-
-    //     std::array<VkAttachmentDescription, 2> attachments = {colorAttachment, depthAttachment};
-    //     VkRenderPassCreateInfo renderPassInfo{};
-    //     renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
-    //     renderPassInfo.attachmentCount = static_cast<uint32_t>(attachments.size());
-    //     renderPassInfo.pAttachments = attachments.data();
-    //     renderPassInfo.subpassCount = 1;
-    //     renderPassInfo.pSubpasses = &subpass;
-    //     renderPassInfo.dependencyCount = 1;
-    //     renderPassInfo.pDependencies = &dependency;
-
-    //     if (vkCreateRenderPass(vlogicaldevice->getDevice(), &renderPassInfo, nullptr, &renderPass) != VK_SUCCESS) 
-    //     {
-    //         throw std::runtime_error("failed to create render pass!");
-    //     }
     // }
 
     void createDescriptorSetLayout() {
